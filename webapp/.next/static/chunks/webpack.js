37545775cd45a26cb7a767f68bcbd4ed0800902b
Ray Copeland <ray@sellingnorthcarolina.com>
2025-04-08 13:35:37 -0400

    Improve VolumeChart component, update database handling, and configure Next.js

--- webapp/.next/static/chunks/webpack.js
+++ webapp/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58bebd564fcc56cd"; }
+/******/ 		__webpack_require__.h = function() { return "28f71d5e5a60fd0c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- webapp/.next/static/webpack/webpack.b6f25c045fe1a633.hot-update.js
+++ webapp/.next/static/webpack/webpack.b6f25c045fe1a633.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "58bebd564fcc56cd"; }
+/******/ 	__webpack_require__.h = function() { return "28f71d5e5a60fd0c"; }
 /******/ }();
 /******/ 
 /******/ }
